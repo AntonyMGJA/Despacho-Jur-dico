63474bde74d2d3808c8242a8392634b7e9c82ff4
AntonyMGJA <114252163+AntonyMGJA@users.noreply.github.com>
2026-06-02 21:04:45 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,7 @@
+this.color = ()=>{
+    var item = document.querySelector('#navv');
+    let el = document.getElementsByClassName('navtrasp');
+        item.classList.toggle('navtrasp');
+     console.log(el)
+    //item.classList.toggle('bg-dark');
+}
